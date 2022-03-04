71aa2ab8bdd45297c168add6fe0f0eb4476a4c76
Domenico De Cristofaro <82721001+DDC253@users.noreply.github.com>
2022-03-04 15:23:54 +0100

    Update try.py

--- try.py
+++ try.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-PAGE_CONFIG = {"page_title":"Align audio data to transcription","page_icon":":smiley:","layout":"centered"}
-st.beta_set_page_config(**PAGE_CONFIG)
 def main():
 	st.title("Audio data time-alignment")
 	st.subheader("retrieve transcription from Word documents")
